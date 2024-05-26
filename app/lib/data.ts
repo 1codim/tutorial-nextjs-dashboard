f73b7643a6c1db0b1b631009e6ae6a36b5f3b4f1
Tayfer <a9036622444@gmail.com>
2024-05-26 19:57:37 +0300

    Убрал Замедление запроса

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,13 +19,13 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // TODO Замедление запроса 1
+    //console.log('Fetching revenue data...');
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
+    // TODO Замедление запроса 2
+    //console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
